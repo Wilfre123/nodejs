f1a153f6500b17030d85c1e7974a34e1572e4684
Wilfre123 <145557794+Wilfre123@users.noreply.github.com>
2025-06-02 20:01:05 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 // Create server
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('<center><h1>Sim dako itlog</h1></center>');
+  res.end('<center><h1>gamay na iya itlog</h1></center>');
 });
 
 // Start listening
