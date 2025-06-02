110d9d4381f05789c9acb21c90f1fa41748d44b1
Moscoso1 <parkwilfrem@gmail.com>
2025-06-02 19:54:39 +0800

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("simbot");
+const http = require('http');
+
+// Define the port
+const PORT = process.env.PORT || 3000;
+
+// Create server
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end('<center><h1>Sim dako itlog</h1></center>');
+});
+
+// Start listening
+server.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
